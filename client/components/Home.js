acd9fbc35ed23defe8d5f6d4d3c787ec8d088012
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2024-04-18 16:19:40 -0400

    successful rendering

---
+++ client/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+/**
+ * COMPONENT
+ */
+export const Home = props => {
+  const {username} = props
+
+  return (
+    <div>
+      <h3>Welcome, {username}</h3>
+      <p>    
+    <span>Score:</span> <span id="score">0</span></p>
+    <canvas width="1920" height="950">
+    </canvas>
+    </div>
+  )
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    username: state.auth.username
+  }
+}
+
+export default connect(mapState)(Home)
---
+++ server/db/db.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
+
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+
+const config = {
+  logging: false
+};
+
+if(process.env.LOGGING === 'true'){
+  delete config.logging
+}
+
+//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+if(process.env.DATABASE_URL){
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+}
+
+const db = new Sequelize(
+  'postgres://postgres:Baseball1027!@localhost:5432/please_work')
+
+db.authenticate().then(() => {
+  console.log('Database connected to server')
+})
+module.exports = db
