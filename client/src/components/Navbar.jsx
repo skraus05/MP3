b4cea78dd1e5110c105799aef20f596f4888a237
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2024-04-23 20:16:10 -0400

    updating spacing between sign up and log in options

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          <Link to="/home">Home </Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -18,8 +18,8 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Login </Link>
+          <Link to="/signup">SignUp</Link>
         </div>
       )}
     </nav>
