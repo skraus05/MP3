e2b16eec917e9d93b76f12c2d684fededf599450
Jacob Daley <larksongdaley@gmail.com>
2024-04-24 18:34:28 -0700

    changed process.env.PORT to 10000 due to render being unable to locate previous port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const { sequelize : db } = require('../models')
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 10000
 const app = require('./app')
 //const seed = require('../script/seed');
 const { connect } = require('./api');
